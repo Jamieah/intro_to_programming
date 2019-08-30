5e4ca452095f50339a773cc1ee5ebc54f7b0f2c7
Jamie Herron <jamieah.jh@gmail.com>
2019-08-30 18:07:20 +0800

    add exercises to basics chapter

---
+++ the_basics/ex3.rb
@@ -0,0 +1,12 @@
+films = { :jaws => 1975,
+          :mummy => 2004,
+          :bond => 2013,
+          :space => 2001,
+          :jones => 1981 
+}
+
+puts films[:jaws]
+puts films[:mummy]
+puts films[:bond]
+puts films[:space]
+puts films[:jones]
